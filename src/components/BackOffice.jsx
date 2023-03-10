486f505d73bcd38d8b11b9a8c778ee5ea2e19e8f
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-10 17:49:12 +0300

    backoffice update :100:

--- src/components/BackOffice.jsx
+++ src/components/BackOffice.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const BackOffice = () => {
@@ -6,8 +6,14 @@ const BackOffice = () => {
   const [year, setYear] = useState("");
   const [type, setType] = useState("");
   const [imgData, setImgData] = useState(null);
+  const [imgName, setImgName] = useState(null);
   const [isError, setIsError] = useState(false);
   const [errMsg, setErrMsg] = useState("");
+
+  const inputRef = useRef(null);
+  const imitateInputTypeFile = () => {
+    inputRef.current.click();
+  };
 
   const url = process.env.REACT_APP_API_URL;
 
@@ -62,6 +68,7 @@ const BackOffice = () => {
     const imgData = new FormData();
     imgData.append("poster", img);
     setImgData(imgData);
+    setImgName(img.name);
   };
 
   const handleSubmit = (e) => {
@@ -108,12 +115,27 @@ const BackOffice = () => {
               />
             </Form.Group>
             <Form.Group className="mb-3">
+              <button
+                type="button"
+                onClick={imitateInputTypeFile}
+                style={{
+                  border: "1px solid white",
+                  padding: "0.25rem 0.5rem",
+                  borderRadius: "0.4rem",
+                }}
+              >
+                <svg viewBox="0 0 24 24" width="24" height="24">
+                  <path d="M16 13a4 4 0 11-4-4 4 4 0 014 4zm6-4v11H2V9a3 3 0 013-3h1.3l1.2-3h9l1.2 3H19a3 3 0 013 3zm-5 4a5 5 0 10-5 5 5 5 0 005-5z"></path>
+                </svg>
+                <span className="ml-2">Pick a Poster</span>
+              </button>
+              <p className="text-success">{imgName}</p>
               <input
                 type="file"
-                // className="d-none"
+                className="d-none"
                 id="avatar"
                 name="avatar"
-                // ref={inputRef}
+                ref={inputRef}
                 accept="image/png, image/jpeg"
                 onChange={handleFileChange}
               ></input>
